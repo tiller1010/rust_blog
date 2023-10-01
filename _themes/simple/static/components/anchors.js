16d01c2172ed7ef79879e8ed13397fe3598a5c31
Tyler Trout <tiller1010@gmail.com>
2023-10-01 18:29:45 -0400

    fix: scroll to current hash

--- _themes/simple/static/components/anchors.js
+++ _themes/simple/static/components/anchors.js
@@ -1,17 +1,24 @@
 export const addAnchorsToHeadings = () => {
+  const header = document.querySelector('header');
   document.querySelectorAll('h2, h3, h4, h5, h6').forEach((heading) => {
     const anchor = document.createElement('a');
     if (!heading.id) {
       heading.id = heading.innerText.toLowerCase().replace(/ /g, '-');
     }
-    anchor.href = '#' + heading.id;
+
+    const anchorHash = '#' + heading.id;
+
+    if (window.location.hash === anchorHash) {
+      scrollTo(heading.offsetLeft, heading.offsetTop - header.offsetHeight - 10);
+    }
+
+    anchor.id = anchorHash;
     anchor.classList.add('heading-anchor');
     anchor.appendChild(heading.cloneNode(true));
     heading.parentNode.replaceChild(anchor, heading);
     anchor.addEventListener('click', (e) => {
       e.preventDefault();
       history.pushState({}, '', anchor.href);
-      const header = document.querySelector('header');
       window.scrollTo(anchor.offsetLeft, anchor.offsetTop - header.offsetHeight - 10);
     });
   });
