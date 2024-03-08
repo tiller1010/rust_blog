0af2a43b9536d3d5274f00fa73354f815418e3ab
Tyler <tiller1010@gmail.com>
2024-03-08 08:23:07 -0500

    fix: update title on page update for analytics

--- _themes/simple/static/components/links.js
+++ _themes/simple/static/components/links.js
@@ -19,6 +19,7 @@ export const addLinkEventListeners = () => {
       e.preventDefault();
       if (typeof window.getPageContent === 'function') {
         getPageContent(link.href);
+        document.title = link.textContent;
       }
       history.pushState({}, '', link.href);
     });
